bc4cb201e1b934cb01cd6da033b919af41124b31
Rodrigo Reyes <rreyes@canaryhealth.com>
2019-08-20 22:27:35 -0500

    Adding some sample modules

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import moment from "moment";
+import { person, sayHello } from './lib';
 
 const greeting = "Hello World";
 console.log(greeting);
@@ -14,3 +15,6 @@ getData("https://jsonplaceholder.typicode.com/posts");
 console.log("Why are we here?");
 var rightNow = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(rightNow);
+
+console.log(person);
+console.log(sayHello('Lucus'));
---
+++ src/lib.js
@@ -0,0 +1,10 @@
+export const person = {
+  name: "Jhon Doe",
+  location: "LA",
+  age: 30
+};
+
+
+export function sayHello(name) {
+  return `Hello ${name}`;
+}
