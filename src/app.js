b22474035505aed7e02d9c818d395b9236d160c2
Rodrigo Reyes <rreyes@canaryhealth.com>
2019-08-20 19:34:27 -0500

    Formatting codefiles

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import moment from 'moment';
+import moment from "moment";
 import regeneratorRuntime from "regenerator-runtime";
 
-const greeting = 'Hello World';
+const greeting = "Hello World";
 console.log(greeting);
 
-const getData = async (url) => {
+const getData = async url => {
   const response = await fetch(url);
   const result = await response.json();
   console.log(result);
 };
 
-getData('https://jsonplaceholder.typicode.com/posts');
+getData("https://jsonplaceholder.typicode.com/posts");
 
-console.log('Why are we here?');
-var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log("Why are we here?");
+var rightNow = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(rightNow);
